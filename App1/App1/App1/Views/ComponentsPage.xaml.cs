631880b77d422d347b87a8ca3e8eded4d68b21b4
Weuller Marcos Da Silva Santos <wmsantos1@stefanini.com>
2016-07-28 14:50:49 -0300

    Projeto Xamarin de treinamento

---
+++ App1/App1/App1/Views/ComponentsPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1.Views
+{
+    public partial class ComponentsPage : ContentPage
+    {
+        public ComponentsPage()
+        {
+            InitializeComponent();
+
+            //Código para setar imagem de acordo com o dispositivo e localização da imagem
+            //imgXmarin.Source = Device.OnPlatform( iOS: ImageSource.FromFile("Images/Xamarin.png"),
+            //                                  Android: ImageSource.FromFile("Xamarin.png"),
+            //                                 WinPhone: ImageSource.FromFile("Assets/Xamarin.png"));
+        }
+
+        void ChangeColor_Clicked(object sender, EventArgs e)
+        {
+            if (bvwColor.Color == Color.Green)
+            {
+                bvwColor.Color = Color.Blue;
+            }
+            else
+            {
+                bvwColor.Color = Color.Green;
+            }
+        }
+    }
+}
---
+++ App1/App1/App1/Views/MyContentPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App1.Views
+{
+    public partial class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {
+            InitializeComponent();
+        }
+
+        void StackLayout_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new StackLayoutPage();
+        }
+
+        void MasterDetail_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new ComponentsPage();
+        }
+
+        void ListView_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new ComponentsPage();
+        }
+
+    }
+}
